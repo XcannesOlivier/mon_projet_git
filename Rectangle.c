d4d5ee22740949776fc20ceb700a92d634f96967
xcannesOlivier <Desruelleolivier@gmail.com>
2025-11-07 16:16:56 +0100

    Ajoute la fonction resetAge(Personne *)

--- Rectangle.c
+++ Rectangle.c
@@ -9,11 +9,16 @@ typedef struct {
 void vieillir(Personne *p) {
     p->age += 1;
 }
+void resetage(Personne *p) {
+    p->age = 0;
+}
 
 int main() {
     Personne p = {"Alice", 25};
     vieillir(&p);
     printf("%s a maintenant %d ans\n", p.nom, p.age);
+    resetage(&p);
+    printf("%s a maintenant %d ans aprés réinitialisation\n", p.nom, p.age);
     return 0;
 }
 
